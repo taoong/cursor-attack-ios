1b3174f3894764e291497a2e0b06f3d3cf3e7815
Tao Ong <taoong@berkeley.edu>
2018-03-30 21:12:39 -0700

    Basic game functions now working, no real enemies.

--- CursorAttack/GameScene.swift
+++ CursorAttack/GameScene.swift
@@ -9,102 +9,87 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    var player = SKSpriteNode()
+    var enemy = SKSpriteNode()
+    var restartBtn = SKSpriteNode()
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var gameOver = false
+    var gameStopped = false
     
     override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+        self.createRestartBtn()
+        physicsWorld.contactDelegate = self
+        player = self.childNode(withName: "player") as! SKSpriteNode
+        enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 1
+        self.physicsBody = border
+    }
+    
+    func createScene() {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if gameOver == false {
+            if contact.bodyA.node == player && contact.bodyB.node == enemy ||
+                contact.bodyB.node == player && contact.bodyA.node == enemy
+            {
+                gameOver = true
+            }
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func createRestartBtn() {
+        restartBtn = SKSpriteNode(imageNamed: "restart_button")
+        restartBtn.size = CGSize(width:100, height:100)
+        restartBtn.position = CGPoint(x: 0, y: 0)
+        restartBtn.zPosition = 6
+        restartBtn.setScale(0)
+        self.addChild(restartBtn)
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    func restartScene() {
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for t in touches {
+            player.run(SKAction.move(to: t.location(in: self), duration: 0.1))
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        if gameStopped == true {
+            for t in touches {
+                let location = t.location(in: self)
+                if restartBtn.contains(location) {
+                    restartBtn.run(SKAction.scale(to: 0.0, duration: 0.3))
+                    gameStopped = false
+                    player.physicsBody?.isDynamic = true
+                    player.physicsBody?.affectedByGravity = false
+                    enemy.physicsBody?.isDynamic = true
+                    player.position = CGPoint(x: 0, y: 0)
+                    enemy.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                    enemy.position = CGPoint(x: -250, y: 500)
+                }
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for t in touches {
+            player.run(SKAction.move(to: t.location(in: self), duration: 0.1))
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        if gameOver == true {
+            restartBtn.run(SKAction.scale(to: 1.0, duration: 0.3))
+            gameOver = false
+            gameStopped = true
+            player.physicsBody?.isDynamic = false
+            enemy.physicsBody?.isDynamic = false
         }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
 }
